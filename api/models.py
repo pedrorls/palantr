c1be989faef6a35aef0b6cdcd26ad2ba55cc5bfd
Pedro Silva <pedro.rls93@gmail.com>
2018-09-05 19:39:14 -0300

    app api created & a model for topic and post

---
+++ api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import Truncator
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    flag = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+
+class Post(models.Model):
+    message = models.CharField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='posts', on_delete='CASCADE')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete='CASCADE')
+
+    def __str__(self):
+        truncated_msg = Truncator(self.message)
+        return truncated_msg.chars(20)
