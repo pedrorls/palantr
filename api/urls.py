6dd1aa4ccc42afb2b010b6820660a2f425662126
Pedro Silva <pedro.rls93@gmail.com>
2018-09-07 18:59:02 -0300

    urls refactored

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('/', home, name='home'),
-    re_path(r'^topics/$', topics_list, name='topics-list'),
-    re_path(r'^topics/(?P<topic_pk>\d+)/$', topic_details, name='topic-details'),
+    path('topics/', topics_list, name='topics-list'),
+    path('topics/<int:topic_pk>/', topic_details, name='topic-details'),
 ]
