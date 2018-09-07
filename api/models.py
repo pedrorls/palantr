0c532e0c0904d24c38f94abdbc46e08a705439f9
Pedro Silva <pedro.rls93@gmail.com>
2018-09-07 19:18:16 -0300

    topic models uses manager to return active only topics

--- api/models.py
+++ api/models.py
@@ -2,18 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import Truncator
 
+class ActiveTopicManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveTopicManager, self).get_queryset().filter(activate=True)
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=30, unique=True)
     activate = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
+    objects = models.Manager()
+    active_objects = ActiveTopicManager()
 
     def __str__(self):
         return self.name
 
     def get_posts_count(self):
         return Post.objects.filter(topic=self).count()
+
 
 
 class Post(models.Model):
