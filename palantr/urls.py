fc0e47f6177aac60e676a7073015bdeed9ed8e13
Pedro Silva <pedro.rls93@gmail.com>
2018-09-09 18:18:08 -0300

    fixed home url

--- palantr/urls.py
+++ palantr/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
